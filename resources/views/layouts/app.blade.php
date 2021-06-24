670192c6b0fa4b71e6ad524c077554698be439a0
BaraaH <mybara96@gmail.com>
2021-06-24 15:26:23 +0300

    change footer links

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -114,14 +114,20 @@
                     <div class="mob_menu_item">
 
                         <ul>
-                            @if(Auth::guest())
-                                <li>
-                                    <a href="<?=url('personal/clubs')?>">Личный кабинет</a>
-                                </li>
-                            @endif
+                            <li>
+                                <a href="<?=url('personal/clubs')?>">Личный кабинет</a>
+                            </li>
+                        @if(Auth::guest())
                             <li>
                                 <a href="{{url('register')}}">Как попасть на LanGame</a>
                             </li>
+                        @else
+                            <li>
+                                <a href="{{url('personal/clubs')}}?action=add_club">Как попасть на LanGame</a>
+                            </li>
+                        @endif
+                            
+                            
                             <li>
                                 <a href="{{url('langame-software')}}">LanGame Software</a>
                             </li>
@@ -209,9 +215,15 @@
                             <li>
                                 <a href="{{url('personal/clubs')}}">Личный кабинет владельца</a>
                             </li>
-                            <li>
-                                <a href="{{url('personal/clubs')}}?action=add_club">Как попасть на Langame</a>
-                            </li>
+                            @if(Auth::guest())
+                                <li>
+                                    <a href="{{url('register')}}">Как попасть на LanGame</a>
+                                </li>
+                            @else
+                                <li>
+                                    <a href="{{url('personal/clubs')}}?action=add_club">Как попасть на LanGame</a>
+                                </li>
+                            @endif
                             <li>
                                 <a href="{{url('langame-software')}}">Langame Software</a>
                             </li>
