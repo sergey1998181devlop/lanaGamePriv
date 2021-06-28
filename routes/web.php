5d650a4196dafbba3ea72ae7c6c31aa27de72c5a
BaraaH <mybara96@gmail.com>
2021-06-28 23:26:04 +0300

    test new mail template

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+Route::get('/test', function () {
+    return view('emails.user.reset');
+});
+
+
 Route::get('/', function () {
     return redirect('moskva');
 });
