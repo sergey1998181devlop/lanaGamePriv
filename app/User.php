16c23cc6ecae34ac781c781367be0844ba5c6f93
BaraaH <mybara96@gmail.com>
2021-06-28 21:12:20 +0300

    test new mail template

--- app/User.php
+++ app/User.php
@@ -5,7 +5,7 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-
+use App\Notifications\ResetPasswordNotification;
 class User extends Authenticatable
 {
     use Notifiable;
