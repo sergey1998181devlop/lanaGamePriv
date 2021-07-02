be30cb6af16a0c50c2ef63618938d9d3c92a10a7
BaraaH <mybara96@gmail.com>
2021-07-02 16:04:58 +0300

    fix error with geo

--- app/Http/Controllers/clubsController.php
+++ app/Http/Controllers/clubsController.php
@@ -17,8 +17,8 @@ class clubsController extends Controller
         $this->isDraft = false;
     }
     public function index($id){
-        $lon = $_COOKIE['lon'];
-        $lat = $_COOKIE['lat'];
+        $lon = isset($_COOKIE['lon']) ? $_COOKIE['lon'] : 0;
+        $lat =isset($_COOKIE['lat']) ? $_COOKIE['lat'] : 0 ;
         $club = club::where('id',$id)->select('*', club::raw('round(1.6 * ( 3959 * acos( cos( radians("'.$lat.'") ) * cos( radians( lat ) ) * cos( radians( lon ) - radians("'.$lon.'") ) + sin( radians("'.$lat.'") ) * sin( radians( lat ) ) ) ),1) AS nearby'))->where('draft','0')->with(array('city' => function($query) {
             $query->select('id','name');
         },'metro'=>function($query) {
