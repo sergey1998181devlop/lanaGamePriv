9fd84b3b57b4b03cca42a1089c43e3b62da72422
baraahamza <mybara96@gmail.com>
2021-08-25 13:46:47 -0700

    test git

--- sqlChanges.sql
+++ sqlChanges.sql
@@ -3,4 +3,4 @@ ALTER TABLE `users` ADD `last_active_at` TIMESTAMP NULL DEFAULT NULL AFTER `upda
 ALTER TABLE `reports` ADD `url` VARCHAR(255) NULL AFTER `message`;
 ALTER TABLE `reports` CHANGE `url` `url` LONGTEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL;
 ALTER TABLE `clubs` ADD `last_admin_edit` INT NULL AFTER `published_by`;
-ALTER TABLE `clubs` ADD `deleted_at` TIMESTAMP NULL AFTER `updated_at`;
+ALTER TABLE `clubs` ADD `deleted_at` TIMESTAMP NULL AFTER `updated_at`;:
