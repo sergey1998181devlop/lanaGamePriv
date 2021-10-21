d74f9bcf932bf7d0f6763a248c407e92de4af007
Kristina Gerasimova <kristina.usova@gmail.com>
2021-10-21 15:35:15 +0300

    fix liked page router

--- resources/views/personal/profile.blade.php
+++ resources/views/personal/profile.blade.php
@@ -40,7 +40,7 @@ function customOldVal($name,$item){
                         </a>
                     @endif
                     @if(player())
-                        <a href="{{url('personal/liked-club')}}">
+                        <a href="{{url('personal/liked')}}">
                             <svg width="34" height="34" viewBox="0 0 34 34" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-svg-like"></use>
                             </svg>
