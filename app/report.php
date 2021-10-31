2e9fe55f9269478a957cb1f526f1005f351cb787
Baraa <mybara96@gmail.com>
2021-10-31 19:32:15 +0300

    hide spam messages

--- app/report.php
+++ app/report.php
@@ -6,13 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class report extends Model
 {
-    // public function newQuery()
-    // {
-    //     $blocked_ips = [
-    //         '54.39.29.64'
-    //     ];
-    //     return parent::newQuery()->whereNotIn('REMOTE_ADDR',['54.39.29.64'])->OrWhereNull('REMOTE_ADDR');
-    // }
     public function ScopeWithoutSpam($query){
         $blocked_ips = [
             '54.39.29.64'
